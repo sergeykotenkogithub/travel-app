f2b446434fc6c2add84b79332de391cda667dac3
sergeykotenko <sergeykotenko@bk.ru>
2022-10-10 09:41:53 +0300

    start app

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../app/components/common/footer/Footer'
+import styles from '../assets/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Footer />
+    </div>
+  )
+}
