020a3ae57854e51c149809d18c4cc27caacbc447
sergeykotenko <sergeykotenko@bk.ru>
2022-10-11 01:57:28 +0300

    add api places and fix rating

--- app/types/place.ts
+++ app/types/place.ts
@@ -7,7 +7,7 @@ export interface IPlace {
     location: TypeLocation
     imagePath: string
     description: string
-    raiting: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+    rating: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
     duration: string
     distance: number
     googleMapLink: string
---
+++ pages/api/places/index.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { IPlace } from '../../../app/types/place'
+
+export const places: IPlace[] = [
+    {
+        slug: 'tokyo',
+        description: 'wefwewegewwefew',
+        imagePath: 'http://localhost:3000/images/places/japan.jpg',
+        location: { country: 'Japan', city: 'Kyoto' },
+        rating: 5,
+        duration: '10 days',
+        distance: 10453,
+        googleMapLink: 'wefewf',
+        mapImage: 'egfw',
+    },
+    {
+        slug: 'venezia',
+        description: 'wefwewegewwefew',
+        imagePath: 'http://localhost:3000/images/places/italy.jpg',
+        location: { country: 'Italy', city: 'Venezia' },
+        rating: 5,
+        duration: '7 days',
+        distance: 9453,
+        googleMapLink: 'wefewf',
+        mapImage: 'egfw',
+    },
+]
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json(places)
+}
